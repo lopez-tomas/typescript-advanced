5f7c80ee5ddc51b7dba848926b7b06ea1f8235c4
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-01 18:30:37 -0300

    [FEAT]: using faker library to generate random products.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,28 @@
-import { addProduct } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: '1',
-  title: 'product test',
-  category: {
-    id: '12',
-    name: 'category test',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  },
-  stock: 90,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-});
+import { products, addProduct } from './products/product.service';
+
+for (let i = 0; i < 50; i++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    title: faker.commerce.productName(),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    price: parseFloat(faker.commerce.price()),
+    stock: faker.datatype.number({min: 10, max: 100}),
+    isNew: faker.datatype.boolean(),
+    image: faker.image.imageUrl(),
+    description: faker.commerce.productDescription(),
+    tags: faker.helpers.arrayElements(),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    color: faker.color.human(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+  });
+}
+
+console.log(products);
