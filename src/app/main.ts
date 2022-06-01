2917df524cebb9e1d02e41ce6a00b09a26459179
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-01 19:40:55 -0300

    [FEAT]: updateProduct imported & tested.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker';
 
-import { products, addProduct, getProduct } from './products/product.service';
+import { products, addProduct, getProduct, updateProduct } from './products/product.service';
 
 for (let i = 0; i < 50; i++) {
   addProduct({
@@ -29,5 +29,45 @@ console.log(products);
 
 const searchProduct = getProduct('id-test');
 const searchProduct2 = getProduct('5bedbb0d-e92e-4681-8711-a38a22ba0c91');
-console.log('searchProduct:', searchProduct);
-console.log('searchProduct2:', searchProduct2);
+console.log('\n\nsearchProduct:\n', searchProduct);
+console.log('\nsearchProduct2:\n', searchProduct2);
+
+const editProduct = updateProduct('id-test', {
+  title: 'New title',
+  category: {
+    id: faker.datatype.uuid(),
+    name: faker.commerce.department(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+  },
+  price: parseFloat(faker.commerce.price()),
+  stock: faker.datatype.number({min: 10, max: 100}),
+  isNew: faker.datatype.boolean(),
+  image: faker.image.imageUrl(),
+  description: faker.commerce.productDescription(),
+  tags: faker.helpers.arrayElements(),
+  size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+  color: faker.color.human(),
+  updatedAt: new Date()
+});
+console.log('\n\neditProduct:\n', editProduct);
+
+const editProduct2 = updateProduct('5bedbb0d-e92e-4681-8711-a38a22ba0c91', {
+  title: 'New title 2',
+  category: {
+    id: faker.datatype.uuid(),
+    name: faker.commerce.department(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+  },
+  price: parseFloat(faker.commerce.price()),
+  stock: faker.datatype.number({min: 10, max: 100}),
+  isNew: faker.datatype.boolean(),
+  image: faker.image.imageUrl(),
+  description: faker.commerce.productDescription(),
+  tags: faker.helpers.arrayElements(),
+  size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+  color: faker.color.human(),
+  updatedAt: new Date()
+});
+console.log('\n\neditProduct2:\n', editProduct2);
