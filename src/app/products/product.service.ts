5b397166d6b1fc592adcbec68a1cb0dd3f2d2207
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-01 18:06:16 -0300

    [FEAT]: learned about readonly which is applied to variables.

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,6 +3,8 @@ import { Product } from './product.model';
 const products: Product[] = [];
 
 const addProduct = (data: Product) => {
+  // data.id = 'fjksdjfkaskf'; // readonly
+  // data.createdAt = new Date(1998, 6, 8); // readonly
   products.push(data);
 }
 
