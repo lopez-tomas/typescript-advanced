c337a9f3b4b425383518be91d3708ad42b0e7624
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-01 19:22:20 -0300

    [FEAT]: getProduct() function imported & tested.

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,10 @@
 import { faker } from '@faker-js/faker';
 
-import { products, addProduct } from './products/product.service';
+import { products, addProduct, getProduct } from './products/product.service';
 
 for (let i = 0; i < 50; i++) {
   addProduct({
-    id: faker.datatype.uuid(),
+    id: i === 0 ? 'id-test' : faker.datatype.uuid(),
     title: faker.commerce.productName(),
     category: {
       id: faker.datatype.uuid(),
@@ -26,3 +26,8 @@ for (let i = 0; i < 50; i++) {
 }
 
 console.log(products);
+
+const searchProduct = getProduct('id-test');
+const searchProduct2 = getProduct('5bedbb0d-e92e-4681-8711-a38a22ba0c91');
+console.log('searchProduct:', searchProduct);
+console.log('searchProduct2:', searchProduct2);
