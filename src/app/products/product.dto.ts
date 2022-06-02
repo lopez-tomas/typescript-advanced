8ab9c3d28b9bc318ecec19206ef9060819a76c50
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-06-02 00:02:19 -0300

    [FEAT]: learned about utily types in TypeScript - Omit & Pick. I used Omit to create a DTO (data transfer object) called CreateProudctDto which extends from Product but OMIT id, createdAt, updatedAt & category; besides it has a new property: categoryId

---
+++ src/app/products/product.dto.ts
@@ -0,0 +1,12 @@
+// DTO = Data Transfer Object
+import { Product } from './product.model';
+
+interface CreateProductDto extends Omit<Product, 'id' | 'createdAt' | 'updatedAt' | 'category'> {
+  categoryId: string;
+}
+
+// type example = Pick<Product, 'color'| 'description'>
+
+export {
+  CreateProductDto,
+}
